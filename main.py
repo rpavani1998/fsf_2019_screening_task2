e9a40f21992d8aa74b81d4573f6c674a4aa5806e
Pavani Rajula <rpavani1998@gmail.com>
2019-03-02 23:47:04 +0530

    Retriving Data Feature from  CSV_File

--- main.py
+++ main.py
@@ -3,16 +3,77 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.factory import Factory
 from kivy.properties import ObjectProperty
 from kivy.uix.popup import Popup
-
+from kivy.properties import ListProperty
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.button import Button
+from kivy.properties import BooleanProperty, ListProperty, StringProperty, ObjectProperty, NumericProperty
+from kivy.uix.recyclegridlayout import RecycleGridLayout
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.base import runTouchApp
+from kivy.lang import Builder
+import pandas as pd
 import os
+from colorama import Fore, Back, Style
+
+class TextInputPopup(Popup):
+    obj = ObjectProperty(None)
+    obj_text = StringProperty("")
+
+    def __init__(self, obj, **kwargs):
+        super(TextInputPopup, self).__init__(**kwargs)
+        self.obj = obj
+        self.obj_text = obj.text
+
+
+class SelectableRecycleGridLayout(FocusBehavior, LayoutSelectionBehavior,
+                                  RecycleGridLayout):
+    ''' Adds selection and focus behaviour to the view. '''
+
+
+class SelectableButton(RecycleDataViewBehavior, Button):
+    ''' Add selection support to the Button '''
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+
+    def refresh_view_attrs(self, root, index, data):
+        ''' Catch and handle the view changes '''
+        self.index = index
+        return super(SelectableButton, self).refresh_view_attrs(root, index, data)
+
+    def on_touch_down(self, touch):
+        ''' Add selection on touch down '''
+        if super(SelectableButton, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos) and self.selectable:
+            return self.parent.select_with_touch(self.index, touch)
+
+    def apply_selection(self, root, index, is_selected):
+        ''' Respond to the selection of items in the view. '''
+        self.selected = is_selected
+
+    def on_press(self):
+        popup = TextInputPopup(self)
+        popup.open()
+
+    def update_changes(self, txt):
+        self.text = txt
+
 
 class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
     cancel = ObjectProperty(None)
 
-class Root(FloatLayout):
+class Root(BoxLayout):
+    data_items = ListProperty([])
     loadfile = ObjectProperty(None)
     text_input = ObjectProperty(None)
+    data_size = NumericProperty(0)
 
     def dismiss_popup(self):
         self._popup.dismiss()
@@ -24,17 +85,34 @@ class Root(FloatLayout):
         self._popup.open()
 
     def load(self, path, filename):
-        with open(os.path.join(path, filename[0])) as stream:
-            self.text_input.text = stream.read()
-
+        self.data_items.clear()
+        print(path, filename)
+        df = pd.read_csv(filename[0])
+        self.data_items.extend(list(df.columns.values))
+        for row in df.iterrows():
+            index, data = row
+            self.data_items.extend(data.tolist())
+            self.data_size = len(data)
         self.dismiss_popup()
+
+    def load_sample(self, filename):
+        df = pd.read_csv(filename[0])
+  
+        for row in df.iterrows():
+            index, data = row
+            self.data_items.extend(data.tolist())
+            self.data_size = len(data)
+    
+    def __init__(self, **kwargs):
+        super(Root, self).__init__(**kwargs)
+        # self.load_sample('/Users/pavanirajula/Desktop',['/Users/pavanirajula/Desktop/C2ImportGroupsSample.csv'])
 
 class MainApp(App):
     pass
 
 Factory.register('Root', cls=Root)
 Factory.register('LoadDialog', cls=LoadDialog)
-
+# Factory.register('LoadData', cls=CSVLayout)
 
 if __name__ == '__main__':
     MainApp().run()
